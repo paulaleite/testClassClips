9b09e0ba016edb0523bfd2c0d10c3452e8304532
paulaleite <paula.t.leite97@gmail.com>
2021-11-12 14:51:45 -0300

    Commented the correct line

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+// app.use('/', express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
